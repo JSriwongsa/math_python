c1dafa3047bb51d1e16aaa43ccf13f944ed8d214
jsriwongsa <jsriwongsa@hawk.iit.edu>
2022-07-26 18:34:02 -0500

    added a while loop to area.py

--- area.py
+++ area.py
@@ -13,28 +13,34 @@ def cal_triangle(width, length, height):
 def cal_circle(radius):
     return(3.14 * (float(radius) * float(radius)))
 
-x = input("Please select your area that you want to calculate. \n A) Square \n B) Recgtangle \n C) Triangle \n D) Circle \n ")
+def input_menu():
+    return input("Please select your area that you want to calculate. \n A) Square \n B) Recgtangle \n C) Triangle \n D) Circle \n ")
 
-if x == "A":
-    length = input("Please input your length: ")
-    result_square = cal_square(length)
-    print("Your area is {} ".format(result_square))
-elif x == "B":
-    width = input("Please input your width: ")
-    length = input("Please input your length: ")
-    result_rectangle = cal_rectangle(width, length)
-    print("Your area is {} ".format(result_rectangle))
-elif x == "C":
-    width = input("Please input your width: ")
-    length = input("Please input your length: ")
-    height = input("Please input your height: ")
-    result_triangle = cal_triangle(width, length, height)
-    print("Your area is {} ".format(result_triangle))
-elif x == "D":
-    radius = input("Please input your radius: ")
-    result_circle = cal_circle(radius)
-    print("Your area is {} ".format(result_circle))   
-else:
-    print("You did not select the choice in the lists that I provide you. Please run the program and select it again!")
-    exit()
+def check_choice(x):
+    if x == "A":
+        length = input("Please input your length: ")
+        result_square = cal_square(length)
+        print("Your area is {} ".format(result_square))
+    elif x == "B":
+        width = input("Please input your width: ")
+        length = input("Please input your length: ")
+        result_rectangle = cal_rectangle(width, length)
+        print("Your area is {} ".format(result_rectangle))
+    elif x == "C":
+        width = input("Please input your width: ")
+        length = input("Please input your length: ")
+        height = input("Please input your height: ")
+        result_triangle = cal_triangle(width, length, height)
+        print("Your area is {} ".format(result_triangle))
+    elif x == "D":
+        radius = input("Please input your radius: ")
+        result_circle = cal_circle(radius)
+        print("Your area is {} ".format(result_circle))
+    else:
+        print("You did not select the choice in the lists that I provide you.")
 
+while True:
+    choice = input_menu()
+    check_choice(choice)
+    if (choice == "A") or (choice == "B") or (choice == "C") or (choice == "D"):
+        break
